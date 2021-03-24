ca9efe7097e07d852d99bfb3fe85c86747ad9554
Adam Jarzębak <adam.jarzebak@external.t-mobile.pl>
2021-03-24 19:14:54 +0100

    add complecity to pass generator

--- sec_utils/secutil.py
+++ sec_utils/secutil.py
@@ -14,17 +14,57 @@ class UserNamespace(object):
 user_namespace = UserNamespace()
 parser = argparse.ArgumentParser(description='Utils')
 parser.add_argument('-l', dest='pass_length', type=int, required=False, help="Password length")
+parser.add_argument('-c', dest='complexity', type=int, required=False,
+                    help="Password complexity: 1 - easy, 2 - medium, 3 - hard")
 parser.parse_known_args(namespace=user_namespace)
 
 
+def generate_easy_pass(pass_length):
+    return ''.join(random.sample(string.ascii_lowercase, pass_length))
+
+
+def generate_medium_pass(pass_length):
+    source = string.ascii_letters + string.digits
+    return ''.join((random.choice(source) for i in range(pass_length)))
+
+
+def generate_hard_pass(pass_length):
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(password_characters) for i in range(pass_length))
+
+
+def validate_complexity_arg():
+    pass_complexity = user_namespace.complexity
+    if pass_complexity is not None:
+        if pass_complexity not in [1, 2, 3]:
+            print("Bad complexity argument\n")
+            print(parser.format_help())
+            sys.exit()
+        return pass_complexity
+    return False
+
+
 def generate_password():
+    pass_complexity = validate_complexity_arg()
     pass_length = user_namespace.pass_length
+    password = None
     if not pass_length:
         pass_length = 20
 
-    password_characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(password_characters) for i in range(pass_length))
-    print("Pass is: ", password)
+    if pass_complexity == 1:
+        password = generate_easy_pass(pass_length)
+    elif pass_complexity == 2:
+        password = generate_medium_pass(pass_length)
+    elif pass_complexity == 3:
+        password = generate_hard_pass(pass_length)
+
+    else:
+        password = generate_medium_pass(pass_length)
+
+    if password:
+        print("Password: ", password)
+    else:
+        print("Could not generate password")
 
 
 class GeneratePassword(argparse.Action):
